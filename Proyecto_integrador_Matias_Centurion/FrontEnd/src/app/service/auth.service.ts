6b5cc2e7e895f5cbdfd019580e2e6308a393e228
MatiasC <mec.058@gmail.com>
2023-02-06 18:44:13 -0300

    auth service

--- Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/service/auth.service.ts
+++ Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/service/auth.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JwtDto } from '../model/jwt-dto';
+import { LoginUsuario } from '../model/login-usuario';
+import { NuevoUsuario } from '../model/nuevo-usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  authURL: 'http://localhost:8080/auth/';
+  authURL = 'http://localhost:8080/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -13,5 +17,7 @@ export class AuthService {
     return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
   }
 
-
+  public login(loginUsuario: LoginUsuario): Observable<JwtDto> {
+    return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario);
+  }
 }
