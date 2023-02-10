77e57c39c434c2bb0eea18ce29c72c81363daec7
MatiasC <mec.058@gmail.com>
2023-02-10 20:39:00 -0300

    interceptor service

---
+++ Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/service/interceptor-service.ts
@@ -0,0 +1,14 @@
+export class InterceptorService {
+    constructor(private tokenService: TokenService){}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let intReq = req;
+        const token = this.tokenService.getToken();
+        if(token != null){
+            intReq = req.clone({
+                headers: req.headers.set('Authorization', 'Bearer ' + token)
+            });
+        }
+        return next.handle(intReq);
+    }
+}
