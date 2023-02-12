625858840dff62890f6fb1f9fe684361900ea41d
MatiasC <mec.058@gmail.com>
2023-02-11 22:38:19 -0300

    experiencia package

---
+++ Proyecto_integrador_Matias_Centurion/BackEnd/src/main/java/com/portfolio/mec/Dto/dtoExperiencia.java
@@ -0,0 +1,38 @@
+package com.portfolio.mec.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class dtoExperiencia {
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+    
+    //Constructores
+    public dtoExperiencia() {
+    }
+
+    public dtoExperiencia(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    
+    //Getter y Setters
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+    
+}
