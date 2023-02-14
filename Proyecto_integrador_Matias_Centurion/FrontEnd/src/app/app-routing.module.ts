0db67ada5c7023ca826665c2a816b5c90d25ce7d
MatiasC <mec.058@gmail.com>
2023-02-14 20:04:15 -0300

    boton agregar exp

--- Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/app-routing.module.ts
+++ Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
+import { NewExperiienciaComponent } from './experiencia/new-experiiencia.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { PanelControlComponent } from './panel-control/panel-control.component';
@@ -9,6 +10,7 @@ import { RegistroComponent } from './registro/registro.component';
 const routes: Routes = [
   {path: '', component:IndexComponent },
   {path: 'login', component:LoginComponent },
+  {path: 'nuevaexp', component:NewExperiienciaComponent},
   {path: 'registro', component:RegistroComponent },
   {path: 'panel-control', component:PanelControlComponent },
   {path: '**', component:ErrorComponent },
