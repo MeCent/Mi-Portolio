61efce57c82046efec8507a4e9ded853a881fe41
MatiasC <mec.058@gmail.com>
2023-03-08 12:19:49 -0300

    experiencia component

--- Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/experiencia/experiencia.component.ts
+++ Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/experiencia/experiencia.component.ts
@@ -6,26 +6,43 @@ import { TokenService } from '../service/token.service';
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
-  styleUrls: ['./experiencia.component.css']
+  styleUrls: ['./experiencia.component.css'],
 })
 export class ExperienciaComponent implements OnInit {
-
   expe: Experiencia[] = [];
 
-  constructor(private sExperiencia: SExperienciaService, private tokenService: TokenService) { }
+  constructor(
+    private sExperiencia: SExperienciaService,
+    private tokenService: TokenService
+  ) {}
 
   isLogged = false;
 
   ngOnInit(): void {
     this.cargarExperiencia();
-    if(this.tokenService.getToken()){
+    if (this.tokenService.getToken()) {
       this.isLogged = true;
-    }else{
+    } else {
       this.isLogged = false;
     }
   }
 
-  cargarExperiencia(): void{
-    this.sExperiencia.lista().subscribe(data => {this.expe = data});
+  cargarExperiencia(): void {
+    this.sExperiencia.lista().subscribe((data) => {
+      this.expe = data;
+    });
+  }
+
+  delete(id?: number) {
+    if (id != undefined) {
+      this.sExperiencia.delete(id).subscribe({
+        next: (data) => {
+          this.cargarExperiencia();
+        },
+        error: (err) => {
+          alert('No se pudo borrar la experiencia');
+        },
+      });
+    }
   }
 }
