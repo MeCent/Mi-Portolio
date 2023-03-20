b8b9e9a8203e3b190188e12be4279aebfb678918
MatiasC <mec.058@gmail.com>
2023-03-20 19:42:44 -0300

    new skill

--- Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/hys/new-hys.component.ts
+++ Proyecto_integrador_Matias_Centurion/FrontEnd/src/app/hys/new-hys.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-hys',
   templateUrl: './new-hys.component.html',
   styleUrls: ['./new-hys.component.css']
 })
-export class NewHysComponent {
+export class NewHysComponent implements OnInit {
+  nombre: string;
+  porcentaje: number;
+
+  constructor(
+    private skills: Skil) {}
+
+  ngOnInit(): void {
+  }
 
 }
